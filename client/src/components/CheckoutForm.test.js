0752635dd4fe35796f0e9443292bd8a68ae3cc80
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-08 03:04:02 -0400

    Test 1 works

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import * as rtl from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+
+    let { getByText } = rtl.render(<CheckoutForm />);
+
+    getByText('Checkout Form');
+
+});
 
 test("form shows success message on submit with form details", () => {});
